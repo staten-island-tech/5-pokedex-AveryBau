f6627603b33f436a864a93ca47fdb56c077565d6
AveryBau <averya34@nycstudents.net>
2026-03-13 09:50:27 -0400

    Update Pokedex.py

--- Pokedex.py
+++ Pokedex.py
@@ -1,13 +1,20 @@
 import json 
 pokedex = open("./pokedex.json", encoding = "utf8")
 data = json.load(pokedex)
-
-hm = int(input("What pokemon you want?"))
-value = data[hm]
+""" 
+choice = int(input("Write name of a pokemon"))
+value = data[choice]
 poke = []
 print("name is", value)
 language = input(("What language?"))
-print(data[hm]["name"][language])
+print(data[choice]["name"][language])
+ """
+new = input("What type of pokemon do you want?")
+list = []
+for mon in data: 
+    if new in mon["type"]:
+        print(mon)
 
-print(data[0]["name"])
-print(data[1]["name"])
+
+""" print(data[0]["name"]
+print(data[1]["name"]) """
